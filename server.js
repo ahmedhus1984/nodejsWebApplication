1eca596bfd1b4b15a0ef1a50c305e9936a8c4d14
hussein <ahmedhus1984@gmail.com>
2025-05-04 11:58:28 +0800

    working web application, with correct layout and response...ie 'back to home' link does not show in homepage, but shows in all other pages

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.set('layout', 'partials/base');
+
+app.get('/', (req, res) => {
+  res.render('index', { layout: 'partials/indexbase' });
+});
+
+app.get('/adduser', (req, res) => {
+  res.render('adduser');
+});
+
+app.listen(3000)
